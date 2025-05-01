a6c93745fc81b4441f205ba2d91d40f8945c3050
Masudur400 <md8004210@gmail.com>
2025-05-01 13:52:43 +0600

    first commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,33 @@
+// Basic Data Type 
+
+// String 
+let firstName : string = 'First Name'
+
+// Number
+let rollNumber : number = 124
+
+// boolean 
+let isAdmin : boolean = true
+
+// undefined 
+let x: undefined = undefined
+
+// null
+let y : null = null
+
+
+let r : number 
+
+r = 123 
+r = 'rana'
+r = null 
+r = true
+
+// Array 
+
+let friends : string[] = ['rana', 'vai']
+let roll : number[] = [12, 45]
+
+// tuple 
+let names : [string, string] = ['rana', 'vai']
+let ageName : [number, string] = [12, 'rana']
---
+++ module1/src/1.5.ts
@@ -0,0 +1,9 @@
+// Reference type ---> Object 
+const user : {
+    firstName : string;
+     middleName ? : string; //optional type
+    lastName: string;
+} = {
+    firstName : 'Masudur', 
+    lastName : 'Rana'
+}
